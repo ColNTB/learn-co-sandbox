35f0a27c870441d7eef3ddf4d8211131196e834c
Nathan Bohn <nathantbohn@gmail.com>
2020-02-03 22:20:30 +0000

    Automatically backed up by Learn

---
+++ looping.rb
@@ -0,0 +1,13 @@
+
+def looping 
+    loop do 
+      score = 1; 
+      puts "Never pass a school bus if it has flashing lights"
+      score++
+      if score>6
+         break 
+      end 
+     end
+end 
+
+looping 
---
+++ test.rb
@@ -0,0 +1,6 @@
+#run_code_inside = false 
+#####puts "Code after if..end"
+
+puts "You know what year it is?"
+this_year = 2019
+puts "Hey, it's not 2019!" unless this_year == 2019
